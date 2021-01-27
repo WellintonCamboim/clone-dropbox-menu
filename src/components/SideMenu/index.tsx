d3f536413751641c47f229a8d364b2da3c93debd
WellintonCamboim <wellinton.camboim@gmail.com>
2021-01-26 21:55:19 -0300

    Aplicando o isActive e scrollY no component SideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Container } from './styles';
 
+const scrollThreshold = 300;
+
 const SideMenu: React.FC = ({children}) => {
-    
+    const [scrollY, setScrollY] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(()=>{
+        function onScroll(){
+            // console.log(window.scrollY);
+            setScrollY(window.scrollY);
+            setIsActive(false);
+        }
+
+        window.addEventListener('scroll', onScroll);
+
+        return () => window.removeEventListener('scroll', onScroll);
+    },[]);
+
+    const classes = [
+        isActive ? 'open' : '',
+        scrollY <= scrollThreshold ? 'scrollOpen' : '',
+    ];
+    //retorna uma string da class acima
+    const className = classes.join(' ').trim();
+
   return (
-  <Container>{children}</Container>
+  <Container className={className}>{children}</Container>
   );
 }
 
--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -20,4 +20,18 @@ export const Container = styled.div`
   @media (min-width: 1440px){
       width: calc(480px + ((100vw - 1440px)/2));
   }
+
+  transform: translateX(100%);
+
+  transition: 0.5s transform cubic-bezier(0.5, 0, 0, 1);
+
+  &.open {
+    transform: translateX(0);
+  }
+
+  @media (min-width: 1024px) {
+    &.scrollOpen {
+      transform: translateX(0);
+    }
+  }
 `;
